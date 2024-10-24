37535f8e7444ce68cd612e6e7d4469838ca07679
Sergio Gracia <sergiograciadiaz@gmail.com>
2024-10-24 11:57:08 +0200

    Create generate-jwt.js

---
+++ generate-jwt.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+
+// Cargar la clave privada desde el archivo generado en el paso del action
+const privateKey = fs.readFileSync('private-key.pem');
+
+// Crear el JWT
+const token = jwt.sign(
+  {
+    // Este token expira en 10 minutos
+    exp: Math.floor(Date.now() / 1000) + 600,
+    iss: process.env.GITHUB_APP_ID, // Usar el ID de la GitHub App desde los secretos
+  },
+  privateKey,
+  { algorithm: 'RS256' }
+);
+
+console.log('Generated JWT:', token);
